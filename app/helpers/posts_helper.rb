d0d706fc79e86f40d5a2a6e834a8d294af8a67d8
Oleh Klym <olklym@softserveinc.com>
2023-01-18 03:08:37 -0500

    Added new functionality for displaying currently  applied filter value

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def filter_by_status
+    %Q(Filter#{params[:status].present? ? "ed" : '' } by: #{params[:status].present? ? params[:status] : "Status"})
+  end
+
+  def should_be_checked?(param, status)
+    return false unless params[param.intern].present?
+    params[param.intern] == status ? true : false
+  end
 end
